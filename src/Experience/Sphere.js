2e73a8d5c9a757141b76de2ad498f902cfd0756b
brunosimon <simon.bruno.77@gmail.com>
2021-10-01 14:06:29 +0200

    :sparkles: Add fresnel

--- src/Experience/Sphere.js
+++ src/Experience/Sphere.js
@@ -12,7 +12,7 @@ export default class Sphere
         this.scene = this.experience.scene
         this.time = this.experience.time
 
-        this.timeFrequency = 0.0001
+        this.timeFrequency = 0.0003
 
         if(this.debug)
         {
@@ -41,10 +41,10 @@ export default class Sphere
         // Light A
         this.lights.a = {}
 
-        this.lights.a.intensity = 1
+        this.lights.a.intensity = 4.5
 
         this.lights.a.color = {}
-        this.lights.a.color.value = '#ff2900'
+        this.lights.a.color.value = '#ff3e00'
         this.lights.a.color.instance = new THREE.Color(this.lights.a.color.value)
 
         this.lights.a.spherical = new THREE.Spherical(1, 0.615, 2.049)
@@ -52,10 +52,10 @@ export default class Sphere
         // Light B
         this.lights.b = {}
 
-        this.lights.b.intensity = 1
+        this.lights.b.intensity = 2
 
         this.lights.b.color = {}
-        this.lights.b.color.value = '#3158ff'
+        this.lights.b.color.value = '#0063ff'
         this.lights.b.color.instance = new THREE.Color(this.lights.b.color.value)
 
         this.lights.b.spherical = new THREE.Spherical(1, 2.561, - 1.844)
@@ -131,17 +131,23 @@ export default class Sphere
             uniforms:
             {
                 uLightAColor: { value: this.lights.a.color.instance },
-                uLightAPosition: { value: new THREE.Vector3(1.0, 1.0, 0.0) },
+                uLightAPosition: { value: new THREE.Vector3(1, 1, 0) },
                 uLightAIntensity: { value: 1 },
 
                 uLightBColor: { value: this.lights.b.color.instance },
-                uLightBPosition: { value: new THREE.Vector3(- 1.0, - 1.0, 0.0) },
+                uLightBPosition: { value: new THREE.Vector3(- 1, - 1, 0) },
                 uLightBIntensity: { value: 1 },
 
-                uDistortionFrequency: { value: 2.0 },
-                uDistortionStrength: { value: 1.0 },
-                uDisplacementFrequency: { value: 2.0 },
-                uDisplacementStrength: { value: 0.2 },
+                uSubdivision: { value: new THREE.Vector2(this.geometry.parameters.widthSegments, this.geometry.parameters.heightSegments) },
+
+                uDistortionFrequency: { value: 1.5 },
+                uDistortionStrength: { value: 0.65 },
+                uDisplacementFrequency: { value: 2.120 },
+                uDisplacementStrength: { value: 0.152 },
+
+                uFresnelOffset: { value: -1.609 },
+                uFresnelMultiplier: { value: 3.587 },
+                uFresnelPower: { value: 1.793 },
 
                 uTime: { value: 0 }
             },
@@ -180,6 +186,24 @@ export default class Sphere
                 this.material.uniforms.uDisplacementStrength,
                 'value',
                 { label: 'uDisplacementStrength', min: 0, max: 1, step: 0.001 }
+            )
+            
+            this.debugFolder.addInput(
+                this.material.uniforms.uFresnelOffset,
+                'value',
+                { label: 'uFresnelOffset', min: - 2, max: 2, step: 0.001 }
+            )
+            
+            this.debugFolder.addInput(
+                this.material.uniforms.uFresnelMultiplier,
+                'value',
+                { label: 'uFresnelMultiplier', min: 0, max: 5, step: 0.001 }
+            )
+            
+            this.debugFolder.addInput(
+                this.material.uniforms.uFresnelPower,
+                'value',
+                { label: 'uFresnelPower', min: 0, max: 5, step: 0.001 }
             )
         }
     }
--- src/Experience/shaders/sphere/vertex.glsl
+++ src/Experience/shaders/sphere/vertex.glsl
@@ -7,10 +7,16 @@ uniform vec3 uLightBColor;
 uniform vec3 uLightBPosition;
 uniform float uLightBIntensity;
 
+uniform vec2 uSubdivision;
+
 uniform float uDistortionFrequency;
 uniform float uDistortionStrength;
 uniform float uDisplacementFrequency;
 uniform float uDisplacementStrength;
+
+uniform float uFresnelOffset;
+uniform float uFresnelMultiplier;
+uniform float uFresnelPower;
 
 uniform float uTime;
 
@@ -42,27 +48,33 @@ void main()
     gl_Position = projectionMatrix * viewPosition;
 
     // Bi tangents
-    float neighbourTangentDistance = (M_PI * 2.0) / 512.0;
-    float neighbourBiTangentDistance = M_PI / 512.0;
+    float distanceA = (M_PI * 2.0) / uSubdivision.x;
+    float distanceB = M_PI / uSubdivision.x;
 
     vec3 biTangent = cross(normal, tangent.xyz);
 
-    vec3 tangentNeighbour = position + tangent.xyz * neighbourTangentDistance;
-    tangentNeighbour = getDisplacedPosition(tangentNeighbour).xyz;
+    vec3 positionA = position + tangent.xyz * distanceA;
+    vec3 displacedPositionA = getDisplacedPosition(positionA).xyz;
 
-    vec3 biTangentNeightbour = position + biTangent.xyz * neighbourBiTangentDistance;
-    biTangentNeightbour = getDisplacedPosition(biTangentNeightbour).xyz;
+    vec3 positionB = position + biTangent.xyz * distanceB;
+    vec3 displacedPositionB = getDisplacedPosition(positionB).xyz;
 
-    vec3 computedNormal = cross(tangentNeighbour, biTangentNeightbour);
+    vec3 computedNormal = cross(displacedPositionA - displacedPosition.xyz, displacedPositionB - displacedPosition.xyz);
     computedNormal = normalize(computedNormal);
 
+    // Fresnel
+    vec3 viewDirection = normalize(displacedPosition.xyz - cameraPosition);
+    float fresnel = uFresnelOffset + (1.0 + dot(viewDirection, computedNormal)) * uFresnelMultiplier;
+    fresnel = pow(fresnel, uFresnelPower);
+
     // Color
-    float lightAIntensity = max(0.0, - dot(normal.xyz, normalize(- uLightAPosition))) * uLightAIntensity;
-    float lightBIntensity = max(0.0, - dot(normal.xyz, normalize(- uLightBPosition))) * uLightBIntensity;
+    float lightAIntensity = max(0.0, - dot(computedNormal.xyz, normalize(- uLightAPosition))) * uLightAIntensity;
+    float lightBIntensity = max(0.0, - dot(computedNormal.xyz, normalize(- uLightBPosition))) * uLightBIntensity;
 
     vec3 color = vec3(0.0);
-    color = mix(color, uLightAColor, lightAIntensity);
-    color = mix(color, uLightBColor, lightBIntensity);
+    // color = mix(color, uLightAColor, fresnel);
+    color = mix(color, uLightAColor, lightAIntensity * fresnel);
+    color = mix(color, uLightBColor, lightBIntensity * fresnel);
 
     // Varying
     vNormal = normal;
